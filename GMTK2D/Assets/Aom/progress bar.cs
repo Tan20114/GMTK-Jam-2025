91e2728faf9a07355689c937650c6439b175c217
Clione1 <chawinbud.exe@gmail.com>
2025-07-31 23:38:45 +0700

    Night timer + UI
    Night timer + UI

---
+++ GMTK2D/Assets/Aom/progress bar.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Pro : MonoBehaviour
+{
+    public Slider timeBar;
+    public float countdownTime = 5f;
+    public Color startColor = Color.green;
+    public Color endColor = Color.red;
+    private float timeLeft;
+    private Image fillImage;
+
+    void Start()
+    {
+        fillImage = timeBar.fillRect.GetComponent<Image>();
+        fillImage.color = startColor;
+        timeLeft = countdownTime;
+        timeBar.maxValue = countdownTime;
+        timeBar.value = countdownTime;
+    }
+    void Update()
+    {
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            timeBar.value = timeLeft;
+
+            // ????????????????????????????????
+            float t = 1f - (timeLeft / countdownTime); // 0 → 1 เมื่อเวลาใกล้หมด
+            fillImage.color = Color.Lerp(startColor, endColor, t);
+        }
+        else
+        {
+            timeBar.value = 0;
+            Debug.Log("Timeout");
+        }
+    }
+}
